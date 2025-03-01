38d659f9bc8d83eadc9f27e74325270605cbd791
Raul Kevin Quispe Diaz <raul.g.quispe@gmail.com>
2025-03-01 15:25:11 -0500

    add

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -6,9 +6,10 @@ import { LoginDataForm } from '@/types/auth';
 import { useForm } from 'react-hook-form';
 import { useLogin } from './useLogin';
 import SpinnerMini from '@/components/SpinnerMini';
+import { useState } from 'react';
 
 export default function LoginForm() {
-    
+    const [showPassword, setShowPassword] = useState(false);
     const initialValues: LoginDataForm = {
         username: '',
         password: '',
@@ -17,12 +18,12 @@ export default function LoginForm() {
         register,
         handleSubmit,
         formState: { errors },
+        watch,
         reset,
     } = useForm({
         defaultValues: initialValues,
     });
     const { autentication, isPending } = useLogin();
-
     const onSubmit = (data: LoginDataForm) => {
         autentication(data, {
             onSuccess: () => {
@@ -32,90 +33,83 @@ export default function LoginForm() {
     };
 
     return (
-        <>
-            <div className='sm:mx-auto sm:w-full sm:max-w-lg'>
-                <form
-                    className='space-y-6'
-                    onSubmit={handleSubmit(onSubmit)}
-                    noValidate>
-                    <div>
-                        <Label htmlFor='username'>Usuario</Label>
-                        <div className='mt-2'>
-                            <Input
-                                type='text'
-                                id='username'
-                                autoComplete='off'
-                                placeholder='usuario'
-                                {...register('username', {
-                                    required: 'El usuario es requerido.',
-                                })}
+        <div className='w-full max-w-sm sm:max-w-md md:max-w-lg mx-auto'>
+            <form
+                className='space-y-6'
+                onSubmit={handleSubmit(onSubmit)}
+                noValidate>
+                <div>
+                    <Label htmlFor='username'>Usuario</Label>
+                    <div className='mt-2'>
+                        <Input
+                            type='text'
+                            id='username'
+                            autoComplete='off'
+                            placeholder='Usuario'
+                            {...register('username', {
+                                required: 'El usuario es requerido.',
+                            })}
+                        />
+                        {errors.username && (
+                            <ErrorMessage>
+                                {errors.username.message}
+                            </ErrorMessage>
+                        )}
+                    </div>
+                </div>
+
+                <div>
+                    <Label htmlFor='password'>Contraseña</Label>
+                    <div className='mt-2'>
+                        <Input
+                            type={showPassword ? 'text' : 'password'}
+                            id='password'
+                            
+                            autoComplete='off'
+                            placeholder='*********'
+                            {...register('password', {
+                                required: 'La contraseña es requerida.',
+                                minLength: {
+                                    value: 8,
+                                    message:
+                                        'La contraseña debe tener al menos 8 caracteres',
+                                },
+                            })}
+                        />
+                        {errors.password && (
+                            <ErrorMessage>
+                                {errors.password.message}
+                            </ErrorMessage>
+                        )}
+                    </div>
+                    <div className='text-sm flex justify-between mt-4'>
+                        <div className='flex items-center'>
+                            <input
+                                type='checkbox'
+                                id='showPassword'
+                                className='h-4 w-4 rounded transition duration-200'
+                                onChange={() => setShowPassword(!showPassword)}
                             />
-                            {errors.username && (
-                                <ErrorMessage>
-                                    {errors.username.message}
-                                </ErrorMessage>
-                            )}
+                            <label
+                                htmlFor='showPassword'
+                                className='ml-2 text-sm'>
+                                Mostrar contraseña
+                            </label>
                         </div>
                     </div>
-                    <div>
-                        <div className='flex items-center justify-start'>
-                            <Label htmlFor='password'>Contraseña</Label>
-                        </div>
-                        <div className='mt-2'>
-                            <Input
-                                type='password'
-                                id='password'
-                                autoComplete='off'
-                                placeholder='*********'
-                                {...register('password', {
-                                    required: 'La contraseña es requerida.',
-                                    minLength: {
-                                        value: 8,
-                                        message:
-                                            'La contraseña debe tener al menos 8 caracteres',
-                                    },
-                                })}
-                            />
-                            {errors.password && (
-                                <ErrorMessage>
-                                    {errors.password.message}
-                                </ErrorMessage>
-                            )}
-                        </div>
-                        <div className='text-sm flex justify-between mt-4'>
-                            <div className='flex items-center'>
-                                <input
-                                    type='checkbox'
-                                    id='showPassword'
-                                    className=' h-3 w-3 rounded transition duration-200'
-                                />
-                                <label
-                                    htmlFor='showPassword'
-                                    className='ml-2 text-sm'> 
-                                    Mostrar contraseñas
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <Button
-                            type='submit'
-                            variant={'principal'}
-                            disabled={isPending}
-                            className='w-[400px] md:w-full text-base'
-                            size={'lg'}>
-                            {!isPending ? (
-                                'Iniciar Sesión'
-                            ) : (
-                                <div className='flex items-center justify-center'>
-                                    {' '}
-                                    <SpinnerMini />
-                                </div>
-                            )}
-                        </Button>
-                    </div>
-                </form>
-            </div>
-        </>
+                </div>
+
+                <div>
+                    <Button
+                        type='submit'
+                        variant='principal'
+                        disabled={isPending}
+                        className='w-full text-base'
+                        size='lg'>
+                        {!isPending ? 'Iniciar Sesión' : <SpinnerMini />}
+                    </Button>
+                </div>
+            </form>
+        </div>
     );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,27 +3,24 @@ import LoginForm from '@/features/auth/LoginForm';
 
 export default function Login() {
     return (
-        <>
-            <div className='h-screen flex items-center justify-center py-20 lg:py-[90px] '>
-                <div className='bg-white rounded-md w-[550px] mx-auto '>
-                    <div className='flex justify-center items-center gap-2 p-5'>
-                        <img src={Logo} alt='logo' className='size-9' />
-                        <span className='font-oleo text-4xl text-teal-600 hidden lg:block'>
-                            Foodie <span className='text-black'>Hub</span>
-                        </span>
+        <div className='h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8'>
+            <div className='bg-white rounded-lg w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl shadow-lg'>
+                <div className='flex flex-col items-center gap-2 p-5'>
+                    <img src={Logo} alt='logo' className='size-9' />
+                    <span className='font-oleo text-3xl md:text-4xl text-teal-600'>
+                        Foodie <span className='text-black'>Hub</span>
+                    </span>
+                </div>
+                <div className='border-b'></div>
+                <div className='flex flex-col justify-center px-6 sm:px-10 pt-5 pb-10'>
+                    <div className='text-center font-poppins'>
+                        <h2 className='text-lg md:text-xl font-semibold leading-9 tracking-tight text-gray-900 mb-4'>
+                            ¡Bienvenido a Foodie Hub!
+                        </h2>
                     </div>
-                    <div className='border-b-2'></div>
-                    <div className='flex min-h-full flex-1 flex-col justify-center px-8 pt-5 pb-10'>
-                        <div className='sm:mx-auto sm:w-full sm:max-w-sm text-center font-poppins'>
-                            <h2 className=' mt-4 text-center text-base md:text-xl font-semibold leading-9 tracking-tight text-gray-900 mb-2'>
-                                ¡Bienvenido a Foodie Hub!
-                            </h2>
-
-                        </div>
-                        <LoginForm />
-                    </div>
+                    <LoginForm />
                 </div>
             </div>
-        </>
+        </div>
     );
 }
